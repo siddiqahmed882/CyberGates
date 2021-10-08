5772298c5d66a441b0b11d7ae3e5e99d21e99d9c
Siddiq <siddiqahmed882@gmail.com>
2021-10-08 22:36:33 +0500

    carousel updated

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,10 +1,20 @@
-$('.testimonials').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000,
-  dots: true,
-});
+if (window.innerWidth < 800) {
+  $('.testimonials').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+  });
+} else {
+  $('.testimonials').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+  });
+}
 
 document.querySelector('.down-arrow').addEventListener('click', (e) => {
   window.scrollBy({
